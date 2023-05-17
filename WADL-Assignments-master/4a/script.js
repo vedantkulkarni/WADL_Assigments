6fb5f431e613a13c59ad45988e8e00a161b62bb6
Vedant <vedantk60@gmail.com>
2023-05-17 23:14:55 +0530

    Initial Commit

---
+++ WADL-Assignments-master/4a/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }});
+  });
